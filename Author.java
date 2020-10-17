67330f3cb017f877bdd2701a30ad706ab435ca90
Vena-Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-17 10:47:40 +0530

    repeated method calling commit

---
+++ Author.java
@@ -0,0 +1,12 @@
+class Book{
+public static void main(String[] args){
+System.out.println("India 2020");
+write();
+write();
+write();
+}
+public static void write(){
+System.out.println("Writter is Abdul Kalam");
+}
+
+}

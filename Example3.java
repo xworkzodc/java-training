f9063eee41819111e17131e9e3e4ec2699324cf0
Vena-Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-20 16:24:58 +0530

    datatype example3 commit

---
+++ Example3.java
@@ -0,0 +1,28 @@
+public class Example3{
+public static void main(String[] args){
+byte continents;
+short medicalGraduatesInIndia;
+int floweringPlantSpecies;
+long numberOfTreesInAmazonForest;
+float percentageOfWaterInHumanody;
+double chargeOfElectron;
+boolean skyIsBlue;
+char gender;
+continents=7;
+System.out.println(continents+" continents");
+medicalGraduatesInIndia=30000;
+System.out.println(medicalGraduatesInIndia+" medical Graduates In India");
+floweringPlantSpecies=400000; 
+System.out.println(floweringPlantSpecies+" flowering Plant Species");
+numberOfTreesInAmazonForest=39000000;
+System.out.println(numberOfTreesInAmazonForest+" trees are there in amazon forest");
+percentageOfWaterInHumanody=60.0f;
+System.out.println(percentageOfWaterInHumanody+" percentage Of Water In Humanody");
+chargeOfElectron=1.60217662;
+System.out.println(chargeOfElectron+" is the charge of electron");
+skyIsBlue=true;
+System.out.println(skyIsBlue);
+gender='F';
+System.out.println(gender);
+}
+}

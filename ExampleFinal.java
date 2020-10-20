bfac1c1b35fab4f94862922379c140c7515ec2fe
Vena-Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-20 16:19:03 +0530

    final keyword commit

---
+++ ExampleFinal.java
@@ -0,0 +1,11 @@
+public class ExampleFinal{
+public static void main(String[] A){
+final String adharnumber="831537624111";
+System.out.println("adharnumber is "+adharnumber);
+final String email;
+email="venakanavi234@gmail.com";
+System.out.println("emailid is "+email);
+final String phoneNumber="7829731400";
+System.out.println("phone number is "+phoneNumber);
+}
+}

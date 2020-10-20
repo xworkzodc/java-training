88dd1556a81f084fb9943ab0cf404fbc24696cc9
Vena-Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-20 16:22:00 +0530

    datatype example1 commit

---
+++ Example1.java
@@ -0,0 +1,28 @@
+public class Example1{
+public static void main(String[] args){
+byte fingers;
+short daysInAYear;
+int numberOfEngineersInIndia;
+long populationInIndia;
+float percentageOfStudent;
+double chargeOfElectron;
+boolean earthIsFlat;
+char grade;
+fingers=5;
+System.out.println(fingers+" fingers in each hand");
+daysInAYear=365;
+System.out.println(daysInAYear+" days in a year");
+numberOfEngineersInIndia=2900000; 
+System.out.println(numberOfEngineersInIndia+" million Engineering students in Indai");
+populationInIndia=1380004385;
+System.out.println(populationInIndia+" population in India");
+percentageOfStudent=83.5f;
+System.out.println(percentageOfStudent+" percentage of student");
+chargeOfElectron=1.60217662;
+System.out.println(chargeOfElectron+" is the charge of electron");
+earthIsFlat=false;
+System.out.println(earthIsFlat);
+grade='A';
+System.out.println(grade);
+}
+}

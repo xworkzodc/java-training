92458698c268960e8359dada4af4df833325a682
Vena-Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-20 16:11:46 +0530

    example on methods commit

---
+++ Examplemethod.java
@@ -0,0 +1,13 @@
+public class Examplemethod{
+public static void main(String[] args){
+System.out.println("invoked main");
+printEmail("venakanavi1998@gmail.com");
+printLang("java");
+}
+public static void printEmail(String printEmail){
+	System.out.println("email id"+printEmail);
+} 
+public static void printLang(String printLang){
+	System.out.println("programming language"+printLang);
+}
+} 

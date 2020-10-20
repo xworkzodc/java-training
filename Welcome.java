af61194a9a41d6c4eafb91e45af7ac4a386af603
Vena-Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-20 16:17:53 +0530

    welcome example commit

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+public class Welcome{
+public static void main(String[] args){
+System.out.println("invoked main");
+printWish("welcome");
+}
+public static void printwish(String printwish){
+	System.out.println("saying "+printwish);
+} 
+
+} 

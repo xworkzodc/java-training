2f9aa82bbbebd0775b87765c894fdda19e09f3ff
Vena-Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-20 16:23:23 +0530

    datatype example2 commit

---
+++ Example2.java
@@ -0,0 +1,28 @@
+public class Example2{
+public static void main(String[] args){
+byte numberOfDaysInMonth;
+short secodsPerHour;
+int populationOfSrilanka;
+long speedOfLight;
+float literacyRateInIndia;
+double valueOfPi;
+boolean earthIsPlanet;
+char bloodGroup;
+numberOfDaysInMonth=31;
+System.out.println(numberOfDaysInMonth+" or 30 days in a month");
+secodsPerHour=3600;
+System.out.println(secodsPerHour+" seconds per hour");
+populationOfSrilanka=21413249; 
+System.out.println(populationOfSrilanka+" population of Srilanka");
+speedOfLight=299792458;
+System.out.println(speedOfLight+" m/s is speed of light");
+literacyRateInIndia=77.7f;
+System.out.println(literacyRateInIndia+" percentage of literacy rate");
+valueOfPi=3.141592;
+System.out.println(valueOfPi+" pi value");
+earthIsPlanet=true;
+System.out.println(earthIsPlanet);
+bloodGroup='A';
+System.out.println(bloodGroup);
+}
+}

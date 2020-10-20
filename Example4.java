da9309e08a1cd85b963a7f91f085d8cb086bee06
Vena-Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-20 16:26:57 +0530

    example 4 on datatype

---
+++ Example4.java
@@ -0,0 +1,28 @@
+public class Example4{
+public static void main(String[] args){
+byte wildlifeSanctuariesInKarnataka;
+short floweringPlantsInIndia;
+int governmentPrimarySchoolsInKarnataka;
+long populationOfChina;
+float milkProductionInIndia;
+double chargeOfElectron;
+boolean weAreInAustralia;
+char result;
+wildlifeSanctuariesInKarnataka=20;
+System.out.println(wildlifeSanctuariesInKarnataka+" wildlife Sanctuaries In Karnataka ");
+floweringPlantsInIndia=20000;
+System.out.println(floweringPlantsInIndia+"flowering Plants In India");
+governmentPrimarySchoolsInKarnataka=44615; 
+System.out.println(governmentPrimarySchoolsInKarnataka+" government schools");
+populationOfChina=1439323776;
+System.out.println(populationOfChina+" population Of China");
+milkProductionInIndia=20.0f;
+System.out.println("India produces milk over "+milkProductionInIndia+" percent of world production");
+chargeOfElectron=1.60217662;
+System.out.println(chargeOfElectron+" is the charge of electron");
+weAreInAustralia=false;
+System.out.println(weAreInAustralia);
+result='P';
+System.out.println(result);
+}
+}

e492c7be8e8745498ce6a9af32973ee9612ff2db
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-22 19:10:38 +0530

    array of states from main method commit

---
+++ States.java
@@ -0,0 +1,22 @@
+public class States{
+
+public static void main(String[] states)
+{
+	
+	System.out.println("JVM invoked main");
+	System.out.println("States in India "+ states);
+	
+	int sizeOfMainParam=states.length;
+	System.out.println("sizeOfMainParam " +sizeOfMainParam);
+	System.out.println("Element @ index 0 "+states[0]);
+	System.out.println("Element @ index 1 "+states[1]);
+	System.out.println("Element @ index 2 "+states[2]);
+	System.out.println("Element @ index 3 "+states[3]);
+	System.out.println("Element @ index 4 "+states[4]);
+	System.out.println("Element @ index 5 "+states[5]);
+	System.out.println("Element @ index 12 "+states[12]);
+	System.out.println("exit main");
+}
+
+
+}

b6e6ac7b878246c3ddfc739cbe39637142794734
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-23 21:32:20 +0530

    other class of main method Zoo commit

---
+++ Forest.java
@@ -0,0 +1,8 @@
+public class Forest{
+	public static void birds(String[] animals){
+	int sizeOfMainParam=animals.length;
+	System.out.println("sizeOfMainParam " +sizeOfMainParam);
+	System.out.println("Element @ index 0 "+animals[0]);
+	System.out.println("Element @ index 1 "+animals[1]);
+	}
+}

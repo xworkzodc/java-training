46f061fd2a07597b2e27124c77fc470438305084
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-23 10:41:27 +0530

    sub class of toy commit

---
+++ Pubg.java
@@ -0,0 +1,8 @@
+public class Pubg{
+	public static void supply(){
+			System.out.println("Pubg has supply");
+	}
+	public static void enemiesAhead(){
+         	System.out.println("Pubg enemiesAhead");
+	}
+}

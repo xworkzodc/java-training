b41669402acd313372dddf66be25a5726db565c6
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-23 21:33:17 +0530

    Main class example2 commit

---
+++ Zoo.java
@@ -0,0 +1,16 @@
+public class Zoo{
+
+public static void main(String[] animals)
+{
+	
+	System.out.println("JVM invoked main");
+	//print refernce representation
+	Forest.birds(animals);
+	System.out.println("animals "+ animals);
+	
+	
+	System.out.println("exit main");
+}
+
+
+}

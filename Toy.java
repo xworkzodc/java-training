e674c67e8047503dab5cdb471d72f3bdfcbe22e7
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-23 10:40:51 +0530

    main class example1 commit

---
+++ Toy.java
@@ -0,0 +1,9 @@
+public class Toy{
+public static void main(String[] game){
+	System.out.println("JVM invoked main");
+	Pubg.supply();
+	Pubg.enemiesAhead();
+	System.out.println("exit main");
+
+}
+}

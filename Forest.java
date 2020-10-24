0b86eeb643e13061c8e64afe756003eaa6a2dcdf
xworkzodc <48981565+xworkzodc@users.noreply.github.com>
2020-10-24 09:13:45 +0530

    Update Forest.java

--- Forest.java
+++ Forest.java
@@ -2,7 +2,7 @@ public class Forest{
 	public static void birds(String[] fav){
 	int sizeOfMainParam=fav.length;
 	System.out.println("sizeOfMainParam " +sizeOfMainParam);
-	System.out.println("Element @ index 0 "+animals[0]);
-	System.out.println("Element @ index 1 "+animals[1]);
+	System.out.println("Element @ index 0 "+fav[0]);
+	System.out.println("Element @ index 1 "+fav[1]);
 	}
 }

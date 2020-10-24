fd3d346a1541a57e338dae6f4f5e61e3f7ff7734
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-24 07:52:02 +0530

    Update Forest.java

--- Forest.java
+++ Forest.java
@@ -1,6 +1,6 @@
 public class Forest{
-	public static void birds(String[] animals){
-	int sizeOfMainParam=animals.length;
+	public static void birds(String[] fav){
+	int sizeOfMainParam=fav.length;
 	System.out.println("sizeOfMainParam " +sizeOfMainParam);
 	System.out.println("Element @ index 0 "+animals[0]);
 	System.out.println("Element @ index 1 "+animals[1]);
